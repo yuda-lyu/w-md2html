df4f2546694965881930f48ca77433adf3d05151
yuda-lyu <firsemisphere@gmail.com>
2025-11-18 19:22:51 +0800

    modify: msg

--- src/htmlClean.mjs
+++ src/htmlClean.mjs
@@ -3,8 +3,11 @@ import * as cheerio from 'cheerio'
 
 
 let htmlClean = (html) => {
+
     let $ = cheerio.load(html)
+
     $('[style*="display:none"]').remove()
+
     return $.html()
 }
 
--- src/htmlRemovePInLi.mjs
+++ src/htmlRemovePInLi.mjs
@@ -3,6 +3,7 @@ import * as cheerio from 'cheerio'
 
 
 let htmlRemovePInLi = (html) => {
+    //因寬鬆清單會於li內產生p元素, 此再轉至docx時會變成使用內文樣式, 無法沿用清單樣式, 故統一刪除各層li下之p元素
 
     let $ = cheerio.load(html, { decodeEntities: false })
 
